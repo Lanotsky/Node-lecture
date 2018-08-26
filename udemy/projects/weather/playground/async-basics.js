aa35d576c2746826247fee86ea3f1a8344331b94
Aaron James Lanot <aaronlanot@gmail.com>
2018-08-26 22:05:07 +0800

    async. And weather app

---
+++ udemy/projects/weather/playground/async-basics.js
@@ -0,0 +1,16 @@
+console.log('start');
+
+// non blocking IO will make it wait 4 sec
+setTimeout(()=>{
+    console.log('FIRING')
+},4000);  
+
+setTimeout(() => {
+    console.log('FIRING-2')
+}, 0);    
+// prints after because settimeout is 
+// non-blocking
+
+console.log('end');
+
+/* what happens in node v8 engine */
